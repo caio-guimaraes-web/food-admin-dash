103cd8f6f9e13a020ac88e5f79e3578feaabeabe
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-03-25 18:08:58 -0300

    API connection - configuring api client

---
+++ src/lib/react-query.ts
@@ -0,0 +1,11 @@
+import { QueryClient } from '@tanstack/react-query'
+
+/* export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: 5000, // 5 seconds
+    },
+  },
+}) */
+export const queryClient = new QueryClient()
