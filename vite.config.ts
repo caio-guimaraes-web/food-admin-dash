01ba104b1d7dedb5b2844e63f86ae777d041cddf
Caio Guimarães <profissional.caioguimaraes@gmail.com>
2025-04-15 09:58:22 -0300

    Tests and Mocks - Setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+/* Importing features to configure vitest inside vite config.
+ * This is a workaround to avoid having to create a separate vitest.config.ts file */
+import type { InlineConfig } from 'vitest/node'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,4 +15,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
